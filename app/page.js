1afae23cb7bec7e1fdc396f4eb299ea3d7ef59d2
Mehdi Shokrani Kooshki <mehdishokrani1@gmail.com>
2024-03-19 12:13:08 -0600

    Some modification added

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            Author{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
